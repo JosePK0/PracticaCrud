4cfb9c2a1dbf38ec38707d1ead0a65635843c09d
JosePK0 <157852544+JosePK0@users.noreply.github.com>
2024-10-17 22:57:05 -0600

    db

--- obj/Debug/net8.0/MiApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9982f55c2108d14c0e5f3a94ad189b63cf265049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad8ce012f862ec0b6f3ec3dc1ab9bafcb603b9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
